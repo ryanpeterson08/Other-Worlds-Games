ae44c93748bcf1a85a4a1fe679ed8c027dc8f238
ryanpeterson08 <ryanpeterson08@gmail.com>
2017-03-09 12:46:34 -0800

    add basic leaflet map

---
+++ src/OtherWorldsGames/wwwroot/js/scripts.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () {
+    var map = L.map('map').setView([45.47, -122.69], 13);
+
+    var tiles = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    ext: 'png'
+    }).addTo(map);
+
+    marker = L.marker([45.477893, -122.695749], 14).addTo(map)
+            .bindPopup("Based out of Hillsdale, in Portland, OR")
+            .openPopup();
+});
